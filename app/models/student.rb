7cd42088e757a235f9e78fb3ca816095765aad3a
lherna1371 <lherna1371@gmail.com>
2013-08-21 17:40:38 -0500

    Update student.rb

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,30 @@
 require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
-# implement your Student model here
+
+  validates :email, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
+  validates :email, uniqueness: true
+  validates :age, :numericality => { greater_than: 5}
+  validates :phone, format: {with: /\.*\d{3}.*\d{3}.*\d{4}/}
+
+  def name
+    first_name +  " " + last_name
+  end 
+  
+  def age
+    now = Date.today
+    age = now.year - birthday.year - ((now.month > birthday.month || (now.month == birthday.month && now.day >= birthday.day)) ? 0 : 1)
+  end
 end
+
+# class EmailValidator < ActiveModel::EachValidator
+#   def validate_each(record, attribute, value)
+#     unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+#       record.errors[attribute] << (options[:message] || "is not an email")
+#     end
+#   end
+# end
+# implement your Student model here
+
+
+
