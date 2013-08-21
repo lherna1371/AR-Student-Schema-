7d5f2aeca8294328a2d81436c3d2e96797b86b50
lherna1371 <lherna1371@gmail.com>
2013-08-21 17:42:11 -0500

    Update 20121011144238_create_students.rb

--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -4,6 +4,16 @@ require_relative '../config'
 
 class CreateStudents < ActiveRecord::Migration
   def change
+    create_table :students do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :gender
+      t.date   :birthday
+      t.string :email
+      t.string :phone 
+
+      t.timestamps
+    end
     # HINT: checkout ActiveRecord::Migration.create_table
   end
 end
